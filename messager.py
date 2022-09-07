4ab0091e138ec41ceeafa9b130b91593ad064255
Quentin HEYLER <quentin.heyler@login-securite.com>
2022-09-07 06:51:18 -0400

    log in file+stdout ok, log error ok

--- messager.py
+++ messager.py
@@ -1,10 +1,9 @@
 # Author : Brother
-import os
 import inspect
 import discord
 import json
-
-#TODO librairie de logs : création d'un fichier de logs sur le fonctionnement+ fichier erreur séparé plutôt que des print
+import logging
+import traceback
 
 # -------------------------------
 # ATTENTION :
@@ -63,56 +62,75 @@ Tu dois ensuite recevoir un message de confirmation du messager sinon, préviens
 client = discord.Client(intents=discord.Intents.default())
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    logger.info(f'{client.user} has connected to Discord!')
 
 @client.event
 async def on_message(message):
-    if not message.author.bot:
-        reload_droit_et_groupe()
-        if len(f"{message.content}") > 0:
-            print(f'channel:{message.channel} message from {message.author}: {message.content}')
+    try :
+        if not message.author.bot:
+            reload_droit_et_groupe()
+            if len(f"{message.content}") > 0:
+                logger.info(f'channel:{message.channel} message from {message.author}: {message.content}')
 
-            m=f"{message.content}".split(">")[1].strip()
-            # on test si il n'y a que le nom du bot dans le message (vide)
-            if len(m) == 0:
-                # on fait peter l'aide
-                await message.channel.send(help_message(str(message.channel)))
-            else:
-                # on récupére les infos du message
-                channel_peuple_source= str(message.channel)
-                cible=f"{message.content}".split(":")[0].split(">")[1].strip()
-                message_txt=":".join(f"{message.content}".split(":")[1:]).strip()
-                
-                # on regarde si c'est un message à destination d'un groupe ou non
-                if cible in groupes_de_discussion:
-                    # on retrouve tout les peuples destinataires
-                    liste_peuple_cible=trouver_les_peuples_du_groupe(channel_peuple_source,cible)
-                    groupe=f"(envoyé à tout {cible}) "
-                    message_de_groupe=True
+                m=f"{message.content}".split(">")[1].strip()
+                # on test si il n'y a que le nom du bot dans le message (vide)
+                if len(m) == 0:
+                    # on fait peter l'aide
+                    logger.info(f"on envoit l'aide pour {message.channel}")
+                    await message.channel.send(help_message(str(message.channel)))
                 else:
-                    # message uniquement pour 1 peuple
-                    liste_peuple_cible=[cible]
-                    groupe=""
-                    message_de_groupe=False
+                    # on récupére les infos du message
+                    channel_peuple_source= str(message.channel)
+                    cible=f"{message.content}".split(":")[0].split(">")[1].strip()
+                    message_txt=":".join(f"{message.content}".split(":")[1:]).strip()
+                    
+                    # on regarde si c'est un message à destination d'un groupe ou non
+                    if cible in groupes_de_discussion:
+                        # on retrouve tout les peuples destinataires
+                        liste_peuple_cible=trouver_les_peuples_du_groupe(channel_peuple_source,cible)
+                        groupe=f"(envoyé à tout {cible}) "
+                        message_de_groupe=True
+                    else:
+                        # message uniquement pour 1 peuple
+                        liste_peuple_cible=[cible]
+                        groupe=""
+                        message_de_groupe=False
 
-                # on envoit à tout les peuples, un par un
-                for channel_peuple_cible in liste_peuple_cible:
-                    message_send=False
-                    message_fail_reason='raison inconnue'
-                    if verifier_droit_parler(str(channel_peuple_source),channel_peuple_cible) or message_de_groupe:
-                        for channel in client.get_all_channels():
-                            # on trouve le channel cible
-                            if channel.name==channel_peuple_cible:
-                                await channel.send( f"Message provenant de {channel_peuple_source} {groupe}: {message_txt}")
-                                resultat=f"Message bien envoyé à {channel_peuple_cible}"
-                                print(resultat)
-                                await message.channel.send(resultat)
-                                message_send=True
-                    else :
-                        message_fail_reason=f"interdit de parler à {channel_peuple_cible}"
-                    if not message_send:
-                        resultat=f"Message non envoyé à {channel_peuple_cible}, à cause de: {message_fail_reason}"
-                        print(resultat)
-                        await message.channel.send(resultat)
+                    # on envoit à tout les peuples, un par un
+                    for channel_peuple_cible in liste_peuple_cible:
+                        message_send=False
+                        message_fail_reason='raison inconnue'
+                        if verifier_droit_parler(str(channel_peuple_source),channel_peuple_cible) or message_de_groupe:
+                            for channel in client.get_all_channels():
+                                # on trouve le channel cible
+                                if channel.name==channel_peuple_cible:
+                                    await channel.send( f"Message provenant de {channel_peuple_source} {groupe}: {message_txt}")
+                                    resultat=f"Message bien envoyé à {channel_peuple_cible} par {channel_peuple_source}"
+                                    logger.info(resultat)
+                                    await message.channel.send(resultat)
+                                    message_send=True
+                        else :
+                            message_fail_reason=f"interdit de parler à {channel_peuple_cible}"
+                        if not message_send:
+                            resultat=f"Message non envoyé à {channel_peuple_cible} par {channel_peuple_source}, à cause de: {message_fail_reason}"
+                            logger.warning(resultat)
+                            await message.channel.send(resultat)
+    except:
+        err = traceback.format_exc().replace("\n", "|")
+        logger.error(err)
 
-client.run(TOKEN)
+# on retrouve le logger de discord
+logger = logging.getLogger('discord')
+
+#on fabrique le bon handler + formatter pour les logs
+handler = logging.FileHandler(filename='bot.log', encoding='utf-8', mode='a+')
+dt_fmt = '%Y-%m-%d %H:%M:%S'
+formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+try:
+    client.run(TOKEN)
+except:
+    err = traceback.format_exc().replace("\n", "|")
+    logger.error(err)
